f2c7e1e7a0d96b397bb9f087514eadb9992ad048
tdninh <tgninh@gmail.com>
2013-12-13 16:44:55 +0700

    Updated

---
+++ TimeStream/Page/AddFriend.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Globalization;
+using Telerik.Windows.Data;
+using Telerik.Windows.Controls.Primitives;
+using Telerik.Windows.Controls;
+
+namespace TimeStream
+{
+    //RadJumpList radJumpList = new RadJumpList();
+    public partial class AddFriend : PhoneApplicationPage
+    {
+        private List<string> GetDays()
+        {
+            List<string> days = new List<string>(12);
+            foreach (string dayName in DateTimeFormatInfo.CurrentInfo.DayNames)
+            {
+                days.Add(dayName);
+            }
+            return days;
+        }
+
+        public AddFriend()
+        {
+            InitializeComponent();
+           
+            List<string> days = this.GetDays();
+            this.radJumpList.ItemsSource = days;
+
+            // add Alphabet item 
+            string alphabet = "#abcdefghijklmnopqrstuvwxyz";
+            List<string> groupPickerItems = new List<string>(32);
+            foreach (char c in alphabet)
+            {
+                groupPickerItems.Add(new string(c, 1));
+            }
+            this.radJumpList.GroupPickerItemsSource = groupPickerItems;
+            // Group by first name
+            GenericGroupDescriptor<string, string>
+            groupByFirstName = new GenericGroupDescriptor<string, string>(day => day.Substring(0, 1).ToLower());
+            this.radJumpList.GroupDescriptors.Add(groupByFirstName);
+        }
+
+        private void radJumpList_GroupPickerItemTap(object sender, GroupPickerItemTapEventArgs e)
+        {
+            // bi loi~ do ko tim ra group tuong ung
+            foreach (DataGroup group in this.radJumpList.Groups)
+            {
+                if (object.Equals(e.DataItem, group.Key))
+                {
+                    e.DataItemToNavigate = group;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ TimeStream/Page/ChatView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Collections.ObjectModel;
+using Telerik.Windows.Controls;
+
+namespace TimeStream
+{
+    public partial class FriendList : PhoneApplicationPage
+    {
+        ObservableCollection<ConversationViewMessage> messageOnChatView = new ObservableCollection<ConversationViewMessage>();
+        public FriendList()
+        {
+            InitializeComponent();
+
+            ConversationViewMessage msg = new ConversationViewMessage("Hello! Than Thanh An", DateTime.Now, ConversationViewMessageType.Outgoing);
+            messageOnChatView.Add(msg);
+            Cons_ChatView.DataContext = messageOnChatView;
+            
+        }
+
+        private void Cons_ChatView_Tap(object sender, ConversationViewMessageEventArgs e)
+        {
+            messageOnChatView.Add(new ConversationViewMessage(Cons_ChatView.Text.ToString(), DateTime.Now, ConversationViewMessageType.Incoming));
+            Cons_ChatView.Text = new string(' ', 1); 
+        }
+    }
+}
